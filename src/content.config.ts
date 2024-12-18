3135c798fa8abc69095c982563232e7f40267a9f
DragonGod65 <pedro.bonifaciodenkel65@gmail.com>
2024-12-18 19:33:11 +0100

    Changed some Names to fit Styling

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,10 +1,8 @@
 import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
+const mods = defineCollection({
+	loader: glob({ base: './src/content/mods', pattern: '**/*.{md,mdx}' }),
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
@@ -17,4 +15,4 @@ const blog = defineCollection({
 	}),
 });
 
-export const collections = { blog };
+export const collections = { mods };
