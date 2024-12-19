88bee38121ab5d828682caeb374b89d3c45cfdc4
DragonGod65 <pedro.bonifaciodenkel65@gmail.com>
2024-12-19 16:57:40 +0100

    Added Astro UI Frameworks
     -> React
     -> Vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,12 @@ import sitemap from '@astrojs/sitemap';
 
 import tailwind from '@astrojs/tailwind';
 
+import react from '@astrojs/react';
+
+import vue from '@astrojs/vue';
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://example.com',
-    integrations: [mdx(), sitemap(), tailwind()],
+    integrations: [mdx(), sitemap(), tailwind(), react(), vue()],
 });
